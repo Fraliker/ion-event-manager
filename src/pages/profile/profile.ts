f89d67fd4bca4035948972f16e6f9d936c6401fe
Leigh Dinaya <leighdinaya04@gmail.com>
2017-08-23 10:39:44 +0800

    add profile page, service, functions

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, NavParams } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth/auth';
+import { ProfileProvider } from '../../providers/profile/profile';
 
 /**
  * Generated class for the ProfilePage page.
@@ -17,11 +19,140 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	public userProfile: any;
+	public birthDate: string;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public authProvider: AuthProvider, 
+  	public profileProvider: ProfileProvider, public navParams: NavParams) {
+  	
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
   }
 
+  ionViewDidEnter(){
+		this.profileProvider.getUserProfile().on('value', userProfileSnapshot => {
+			this.userProfile = userProfileSnapshot.val();
+			this.birthDate = userProfileSnapshot.val().birthDate;
+		});
+  }
+
+	//logout function
+	logOut(): void {
+		this.authProvider.logoutUser().then(() => {
+			this.navCtrl.setRoot('login');
+		});
+	}
+
+	//function to update user's first and last name
+	updateName(){
+		const alert = this.alertCtrl.create({
+			message: 	"Your first name and last name",
+			inputs: [
+				{ 
+					name: 'firstName',
+					placeholder: 'Your first name',
+					value: this.userProfile.firstName
+				},
+				{
+					name: 'lastName',
+					placeholder: 'Your last name',
+					value: this.userProfile.lastName;
+				},
+			],
+			buttons: [
+				{
+					text: 'Cancel'
+				},
+				{
+					text: 'Save',
+					handler: data => {
+						this.profileProvider.updateName(data.firstName, data.lastName);
+					}
+				}
+			]
+		});
+		alert.present();
+	}
+
+	//function to update birthdate
+	updateDOB(birthDate){
+		this.profileProvider.updateDOB(birthDate);
+	}
+
+	//function to update user's email
+	updateEmail(){
+		const alert = this.alertCtrl.create({
+			message: 	"Your first name and last name",
+			inputs: [
+				{ 
+					name: 'newEmail',
+					placeholder: 'Your new email address',
+				},
+				{
+					name: 'password',
+					placeholder: 'Your password',
+					type: 'password'
+				},
+			],
+			buttons: [
+				{
+					text: 'Cancel'
+				},
+				{
+					text: 'Save',
+					handler: data => {
+						const newEmail = data.newEmail;
+
+						this.profileProvider.updateEmail(data.newEmail, data.password)
+							.then( () => {
+								this.userProfile.email = newEmail;
+							}).catch(error => {
+								console.log('ERROR: ' + error.message);
+							});
+					}
+				}
+			]
+		});
+		alert.present();
+	}
+
+	//function to update user's password
+	updatePassword(){
+		const alert = this.alertCtrl.create({
+			inputs: [
+				{ 
+					name: 'newPassword',
+					placeholder: 'Your new password',
+					type: 'password'
+				},
+				{
+					name: 'oldPassword',
+					placeholder: 'Your old password',
+					type: 'password'
+				},
+			],
+			buttons: [
+				{
+					text: 'Cancel'
+				},
+				{
+					text: 'Save',
+					handler: data => {
+						const newPassword = data.newPassword;
+
+						this.profileProvider.updateEmail(data.newPassword, data.oldPassword)
+							.then( () => {
+								this.userProfile.password = newPassword;
+							}).catch(error => {
+								console.log('ERROR: ' + error.message);
+							});
+					}
+				}
+			]
+		});
+		alert.present();
+	}
+	
 }
--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import firebase from 'firebase';
 
 /*
   Generated class for the ProfileProvider provider.
@@ -11,8 +12,65 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProfileProvider {
 
+	public userProfile:firebase.database.Reference;
+	public currentUser:firebase.User;
+
   constructor(public http: Http) {
     console.log('Hello ProfileProvider Provider');
+    firebase.auth().onAuthStateChanged( user => {
+  		if(user){
+  			this.currentUser = user;
+  			this.userProfile = firebase.database().ref('/userProfile').child(user.uid);
+  		}
+  	});
   }
+
+   //function that will return user profile
+  getUserProfile(): firebase.database.Reference {
+  	return this.userProfile;
+  }
+
+  //funtion that will update user's name and last name
+  updateName(firstName: string, lastName: string): firebase.Promise<void> {
+		return this.userProfile.update({
+			firstName: firstName,
+			lastName: lastName,
+		});
+	}
+
+	//function that will update user's date of birth
+	updateDOB(birthDate: string): firebase.Promise<any> {
+		return this.userProfile.update({
+			birthDate: birthDate,
+		});
+	}
+
+	//function that will change user's email address
+	updateEmail(newEmail: string, password: string): firebase.Promise<any> {
+		const credential = firebase.auth.EmailAuthProvider
+			.credential(this.currentUser.email, password);
+				return this.currentUser.reauthenticateWithCredential(credential)
+					.then( user => {
+						this.currentUser.updateEmail(newEmail)
+					.then( user => {
+						this.userProfile.update({ email: newEmail });
+					}
+				);
+			}
+		);
+	}
+
+	//function that will update user's password
+	updatePassword(newPassword: string, oldPassword:string):firebase.Promise<any>{
+		const credential = firebase.auth.EmailAuthProvider.credential(this.currentUser.email, oldPassword);
+		return this.currentUser.reauthenticateWithCredential(credential)
+			.then( user => {
+				this.currentUser.updatePassword(newPassword).then( user => {
+					console.log("Password Changed");
+			}, error => {
+				console.log(error);
+			});
+		});
+	}
 
 }
