e92c8d652b3ec1ab39d2c95991956c1f0c2d3c04
Leigh Dinaya <leighdinaya04@gmail.com>
2017-08-24 06:37:23 +0800

    work on event detail page, show event details, add guest list, take photo of the guest

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EventProvider } from '../../providers/event/event';
-
+import { Camera } from '@ionic-native/camera';
 /**
  * Generated class for the EventDetailPage page.
  *
@@ -20,8 +20,11 @@ import { EventProvider } from '../../providers/event/event';
 export class EventDetailPage {
 
 	public currentEvent: any;
+	public guestPicture: string = null
+	public guestName: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider, 
+  	public cameraPlugin: Camera) {
 
   }
 
@@ -36,4 +39,28 @@ export class EventDetailPage {
 		});
 	}
 
+	addGuest(guestName){
+		this.eventProvider.addGuest(guestName, this.currentEvent.id, this.currentEvent.price, this.guestPicture)
+			.then( () => {
+				this.guestName = '';
+				this.guestPicture = null;
+			});
+	}
+
+	takePicture(){
+		this.cameraPlugin.getPicture({
+			quality : 95,
+			destinationType : this.cameraPlugin.DestinationType.DATA_URL,
+			sourceType : this.cameraPlugin.PictureSourceType.CAMERA,
+			allowEdit : true,
+			encodingType: this.cameraPlugin.EncodingType.PNG,
+			targetWidth: 500,
+			targetHeight: 500,
+			saveToPhotoAlbum: true
+			}).then(imageData => {
+				this.guestPicture = imageData;
+			}, error => {
+				console.log("ERROR -> " + JSON.stringify(error));
+		});
+	}
 }
--- src/providers/event/event.ts
+++ src/providers/event/event.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import firebase from 'firebase';
+import firebase  from 'firebase';
 
 /*
   Generated class for the EventProvider provider.
@@ -13,6 +13,7 @@ import firebase from 'firebase';
 export class EventProvider {
 
 	public userProfileRef:firebase.database.Reference;
+  public guestName: string;
 
   constructor(public http: Http) {
     console.log('Hello EventProvider Provider');
@@ -30,6 +31,7 @@ export class EventProvider {
   		date: eventDate,
   		price: eventPrice * 1,
   		cost: eventCost * 1,
+      revenue: eventCost * -1
   	});
   }
 
@@ -42,5 +44,29 @@ export class EventProvider {
   getEventDetail(eventId: string): firebase.database.Reference {
   	return this.userProfileRef.child('/eventList').child(eventId);
   }
+
+  //function that will add guest to event
+  addGuest(guestName, eventId, eventPrice, guestPicture = null): firebase.Promise<any>{
+    return this.userProfileRef.child('/eventList').child(eventId)
+      .child('guestList').push({ guestName: guestName })
+      .then( newGuest => {
+        this.userProfileRef.child('/eventList').child(eventId)
+          .transaction( event => {
+            event.revenue += eventPrice;
+             return event;
+          });
+         if (guestPicture != null) {
+          firebase.storage().ref('/guestProfile/').child(newGuest.key)
+            .child('profilePicture.png')
+            .putString(guestPicture, 'base64', {contentType: 'image/png'})
+            .then((savedPicture) => {
+              this.userProfileRef.child('/eventList').child(eventId).child('guestList')
+                .child(newGuest.key).child('profilePicture').set(savedPicture.downloadURL);
+            });
+         }
+      });
+  }
+
+
 
 }
